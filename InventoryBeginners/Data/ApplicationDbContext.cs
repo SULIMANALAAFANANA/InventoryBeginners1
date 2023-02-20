fe2334e077fd43e21a15989ee4479ed141a0ff2b
HP <HP@DESKTOP-VP5V6ES>
2023-02-20 11:17:16 +0200

    Add project files.

---
+++ InventoryBeginners/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using InventoryBeginners.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryBeginners.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public virtual DbSet<Unit> Units { get; set; }
+    }
+}
